301e8d881b34637fe0fb7d7534609e812dc97fdd
Pano Papadatos <pano@heypano.com>
2023-03-06 17:11:53 -0500

    feat: after import from pupds working

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,31 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import type { AppProps } from "next/app";
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyles = createGlobalStyle`
+  html, body {
+    padding: 0;
+    margin: 0;
+  }
+
+  html, body, #__next{
+    height: 100%;
+  }
+
+  a {
+    color: inherit;
+    text-decoration: none;
+  }
+  
+  * {
+      box-sizing: border-box;
+  }
+`;
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <GlobalStyles />
+      <Component {...pageProps} />
+    </>
+  );
 }
